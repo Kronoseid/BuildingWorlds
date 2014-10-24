cebb613768c6298cc98b511da6604a6985494686
Brian <tsukb234@newschool.edu>
2014-10-24 12:30:40 -0400

    Initial Git Test

---
+++ Assets/scripts/nycProcedure.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nycProcedure : MonoBehaviour {
+
+	public GameObject firstThing;
+	public GameObject secondThing;
+	public GameObject thirdThing;
+	public GameObject fourthThing;
+	public int counter;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (NewYorkMaker);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator NewYorkMaker(){
+
+	}
+}
